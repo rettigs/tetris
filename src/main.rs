53389e5e1f4d66db889417ef818f8ac2f51f98ee
Sean Rettig <seanrettig@gmail.com>
2015-12-23 15:43:02 -0800

    now using piston to draw

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+extern crate piston_window;
+
+use piston_window::*;
+
 struct Board {
     width: usize,
     height: usize,
@@ -11,9 +15,9 @@ impl Board {
             height: height,
             tiles: vec![vec![false; width]; height],
         };
-        for y in (0..board.height) {
+        for y in 0..board.height {
             let mut row = Vec::new();
-            for x in (0..board.width) {
+            for x in 0..board.width {
                 row.push(false);
             }
             board.tiles.push(row);
@@ -22,9 +26,9 @@ impl Board {
     }
 
     fn print(&self) {
-        for y in (0..self.height) {
+        for y in 0..self.height {
             let mut row = "".to_owned();
-            for x in (0..self.width) {
+            for x in 0..self.width {
                 if self.get(x, y) {
                     row = row + "#";
                 } else {
@@ -45,6 +49,23 @@ impl Board {
 }
 
 fn main() {
-    let board = Board::new(10, 10);
-    board.print();
+    let mut board = Board::new(10, 20);
+    board.set(1, 1, true);
+    let window: PistonWindow =
+        WindowSettings::new("Tetris", [board.width as u32 * 16, board.height as u32 * 16])
+        .exit_on_esc(true).build().unwrap();
+    for e in window {
+        e.draw_2d(|c, g| {
+            clear([0.0; 4], g);
+            for y in 0..board.height {
+                for x in 0..board.width {
+                    if board.get(x, y) {
+                        rectangle([1.0, 0.0, 0.0, 1.0], // red
+                                  [x as f64 * 16.0, y as f64 * 16.0, 16.0, 16.0],
+                                  c.transform, g);
+                    }
+                }
+            }
+        });
+    }
 }
